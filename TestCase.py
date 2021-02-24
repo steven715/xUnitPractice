b97a5a96cb2f880636a3997d8248258b5224db44
unknown <107153@ski.ad>
2021-02-24 15:19:38 +0800

    First section

---
+++ TestCase.py
@@ -0,0 +1,66 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        result = TestResult()
+        result.testStarted()
+        self.setUp()
+        method = getattr(self, self.name)
+        method()
+        self.tearDown()
+        return result
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 1
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+
+    def setUp(self):
+        self.log = "setUp "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
+    def testBrokenMethod(self):
+        raise Exception
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert ("setUp testMethod tearDown " == test.log)
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert ("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert ("1 run, 1 failed", result.summary)
+
+TestCaseTest("testTemplateMethod").run()
